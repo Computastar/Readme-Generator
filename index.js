5af53b26adeb1d3f9ef9c544a81907e7117c2e7f
Computastar <jason.hunter@computastar.com>
2023-02-17 14:48:01 +0000

    Build out first prompt for github name

--- index.js
+++ index.js
@@ -3,8 +3,27 @@ const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 
+const questions = [    
+    {
+type: 'input',
+message: 'Please add your GitHub profile:',
+name: 'github'
+}
+]
 
 // TODO: Create a function to write README file
+function init()
+{
+    inquirer
+    .prompt(questions)
+    .then((response) => {
+        console.log(response)
+
+    })
+}
+
+
 
 // TODO: Create a function to initialize app
 
+init();
