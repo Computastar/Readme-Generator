66d39b20144f7ad67f36d6fbcb91a204f144f2d2
Computastar <jason.hunter@computastar.com>
2023-02-17 14:39:02 +0000

    Added skelton TODO

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const inquirer = require('inquirer')
 const fs = require('fs');
 
+// TODO: Create an array of questions for user input
+
+
+// TODO: Create a function to write README file
+
+// TODO: Create a function to initialize app
+
